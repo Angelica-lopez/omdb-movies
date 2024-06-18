f4dd7b75715fc7f52e7680a90ebd2290067ffbc3
Angelica-lopez <angielopez4004@gmail.com>
2024-06-18 15:40:52 -0500

    Implemented movie search with loading and empty states

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import { useEffect, useState } from "react";
 import StringCleaner from "./components/StringCleaner";
 import { fetchMovies } from "./services/api";
 import Card from "./components/Card";
+import LoadingState from "./assets/images/loading-state.png";
 
 const App = () => {
   const [movies, setMovies] = useState(null);
+  const [inputValue, setInputValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [movieSearchTerm, setMovieSearchTerm] = useState("end");
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   useEffect(() => {
     const getMovies = async () => {
+      setLoading(true);
+
       try {
-        const fetchedData = await fetchMovies("end");
+        const fetchedData = await fetchMovies(movieSearchTerm);
         setMovies(fetchedData.Search);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
     getMovies();
-  }, []);
+  }, [movieSearchTerm]);
 
   return (
     <div>
@@ -26,17 +37,52 @@ const App = () => {
       </header>
       <div className='flex flex-col gap-24 mt-7'>
         <StringCleaner />
-        <div class='grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-6 px-4 md:px-24 mx-auto'>
-          {movies?.map(({ Title, Year, Poster, Type, imdbID }) => (
-            <Card
-              title={Title}
-              image={Poster}
-              year={Year}
-              type={Type}
-              key={imdbID}
+
+        <div className='mx-auto w-full max-w-[1000px]'>
+          <div className='mb-4 w-full'>
+            <input
+              type='text'
+              id='stringInput'
+              value={inputValue}
+              className='border-2 border-grey-500 px-2 rounded-md'
+              onChange={handleInputChange}
             />
-          ))}
+            <button
+              onClick={() => setMovieSearchTerm(inputValue)}
+              className='bg-[#6FBED6] border-2 border-white text-white ml-2 py-[2px] rounded-md text-sm px-2'
+            >
+              Search
+            </button>
+          </div>
+          {loading ? (
+            <img
+              src={LoadingState}
+              alt='Loading image'
+              className='animate-spin w-[50px] h-[50px]'
+            />
+          ) : (
+            <>
+              {!movies?.length ? (
+                <h1 className='text-md text-white font-medium text-center my-12'>
+                  Oops! No movies found. Try a different search.
+                </h1>
+              ) : (
+                <div class='grid grid-cols-1  md:grid-cols-3 lg:grid-cols-4 gap-6 '>
+                  {movies?.map(({ Title, Year, Poster, Type, imdbID }) => (
+                    <Card
+                      title={Title}
+                      image={Poster}
+                      year={Year}
+                      type={Type}
+                      key={imdbID}
+                    />
+                  ))}
+                </div>
+              )}
+            </>
+          )}
         </div>
+
         <div class='rounded bg-white flex flex-col p-3 gap-3 w-full max-w-[1000px] m-auto'>
           <div className='flex gap-3'>
             <div className='w-full bg-[#6FBED6] text-white font-bold rounded h-[70px] flex items-center justify-center'>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,13 @@
+import SadFace from "../../assets/images/sad-face.jpeg";
+
 const Card = ({ image, title, year, type }) => {
   return (
     <div className='rounded-md p-6 bg-white flex flex-col gap-4  items-center max-w-[350px] justify-between'>
       <div className='flex flex-col gap-4'>
         <img
           className='rounded-md w-[250px] h-[280px] m-auto'
-          src={image}
-          alt={title}
+          src={image === "N/A" ? SadFace : image}
+          alt='Movie poster'
         />
         <h1 className='text-[#6FBED6] font-semibold text-lg'>{title}</h1>
       </div>
